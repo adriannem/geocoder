7705e8cd8ef9903ea6c7ce6f3ba3a4771d7ddd65
Adrianne M <affirmwealth@gmail.com>
2014-07-29 08:40:08 -0500

    configured twitter bootstrap, added db seeds

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,12 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'locations#new'
+
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  # get 'products/:id' => 'catalog#view'
+
+  # get 'locations/new:format' => 'locations#new'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Location.create(address: 'Start Houston, Houston, TX', latitude: '30.007182', longitude: '-95.3797411')
+Location.create(address: 'Houston Zoo, Houston, TX', latitude: '29.7119104', longitude: '-95.392828')
+Location.create(address: 'Hobby Airport, Houston, TX', latitude: '29.6541074', longitude: '-95.2766145')
+Location.create(address: 'Alley Theater, Houston, TX', latitude: '29.7620938', longitude: '-95.3653724')
+
+
